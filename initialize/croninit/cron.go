19bf06dbfd3d9a0973e4df5bd6dd7d3d266f9f5c
lieoxc <xiaocongli45@gmail.com>
2025-07-05 21:46:08 +0800

    数据清理日志修改为30天

--- initialize/croninit/cron.go
+++ initialize/croninit/cron.go
@@ -29,8 +29,8 @@ func CronInit() {
 	// 初始化设备统计定时任务
 	//InitDeviceStatsCron(c)
 
-	// 每天凌晨2点执行数据清理
-	c.AddFunc("0 2 * * *", func() {
+	// 间隔30min 执行数据清理
+	c.AddFunc("0 */5 * * * *", func() {
 		logrus.Debug("系统数据清理任务开始：")
 		service.GroupApp.CleanSystemDataByCron()
 	})
